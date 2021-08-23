d117ad95236e43b6e8e9c106b533ea7db3b6f3ac
rafael-tivane <luislangabusiness@gmail.com>
2021-08-23 08:32:57 +0100

    Finishing form validation

---
+++ src/components/Form/styles.js
@@ -0,0 +1,93 @@
+import { makeStyles } from "@material-ui/core";
+import bgPattern from '../../assets/images/patterns/pattern-lines.svg'
+import { codGrey } from '../../styles/colors.js';
+
+const useStyles = makeStyles(theme => ({
+    bookingForm: {
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+        margin: '0 auto',
+        padding: '1rem 5% 2rem 5%',
+        transform: 'translateY(-94px)',
+        width: '90%',
+        [theme.breakpoints.up('sm')]: {
+            width: '70%'
+        },
+        [theme.breakpoints.up('md')]: {
+            width: '50%'
+        },
+        [theme.breakpoints.up('lg')]: {
+            position: 'absolute',
+            right: '10%',
+            top: 0,
+            transform: 'translate(0%, -50%)',
+            width: 350,
+            transformStyle: 'preserve-3d',
+            '&::after': {
+                backgroundImage: `url(${bgPattern})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 170,
+                bottom: '-11%',
+                content: '""',
+                height: 100,
+                left: '-14%',
+                position: 'absolute',
+                width: 180,
+                transform: 'translateZ(-1px)'
+            }
+        },
+        [theme.breakpoints.up(1500)]: {
+            right: '15%'
+        }
+    },
+    paper: {
+        marginTop: '1.3rem'
+    },
+    bookingLabel: {
+        fontSize: '.95rem',
+        fontWeight: 700
+    },
+    bookingFormPaper: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    bookingInput: {
+        width: '31%'
+    }, 
+    bookingActionsContainer: {
+        borderBottom: `1px solid ${codGrey}`
+    },
+    bookingActionButton: {
+        fontSize: '1.2rem'
+    },
+    bookingGuestsLabel: {
+        color: codGrey,
+        fontSize: '.95rem'
+    },
+    bookingSumbit: {
+        backgroundColor: codGrey,
+        fontSize: '.9rem',
+        width: '100%',
+        '&:hover': {
+            border: `1px solid ${codGrey}`
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: '70%'
+        },
+        [theme.breakpoints.up('md')]: {
+            margin: '1.5rem auto 0 auto',
+            width: '70%'
+        }
+    },
+    emailSuggestion: {
+        color: 'red',
+        marginLeft: 3
+    },
+    emailSuggestionHighlight: {
+        color: '#000',
+        textDecoration: 'underline'
+    }
+}));
+
+export {
+    useStyles
+}
--- src/pages/Booking/index.jsx
+++ src/pages/Booking/index.jsx
@@ -1,12 +1,10 @@
-import { Container, Typography, Grid, Paper, Link, TextField, MenuItem, Button  } from '@material-ui/core';
+import { Container, Typography, Grid, Paper, Link } from '@material-ui/core';
 import Footer from '../../components/Footer';
 import { useBackground, useDisplay, useResponsive, useTypography, useServices } from '../../styles';
-import Mailcheck from "../../assets/js/mailcheck";
 import { useStyles } from './styles.js';
 import classNames from 'classnames';
-import { useState, useEffect } from 'react';
-import { useForm, Controller  } from "react-hook-form";
-import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+import Form from '../../components/Form';
 
 const Booking = () => {
     const preventDefault = (event) => event.preventDefault();
@@ -16,91 +14,6 @@ const Booking = () => {
     const responsive = useResponsive();
     const text = useTypography();
     const services = useServices();
-
-    const options = [{ value: 'AM', label: 'AM'}, { value: 'PM', label: 'PM'}];
-    const [ guests, setGuests ] = useState(1); 
-    const [ dayTime, setDayTime ] = useState('AM');
-    const [ emailSuggestions, setEmailSuggestions ] = useState('');
-
-    const requiredMessage = 'This field is required';
-    const { register, handleSubmit, formState: { errors, isValid }, reset, setError, control, setValue, clearErrors } = useForm({  mode: 'onBlur', 
-        reValidateMode: 'onBlur', shouldUnregister: false });
-
-    const history = useHistory();
-
-    const decresaeGuestsNumber = () => {
-        let number = guests  - 1;
-        if(number === 0) {
-            number = 1;
-        }
-        setGuests(g => number);
-    };
-
-    const incresaeGuestsNumber = () => {
-        let number = guests  + 1;
-
-        if(number === 8) {
-            number = 7;
-        }
-        setGuests(g => number);
-    };
-
-    const onSubmit = data => {
-        if(isValid) {
-            data = { ...data, guests: guests};
-            reset();
-            history.push('/')
-        }
-    };
-
-    const domains = ['gmail.com', 'aol.com', 'hotmail.com', 'yahoo.com', 'outlook.com', 'live.com', 'msn.com'];
-    const topLevelDomains = ["com", "net", "org"];
-    const emailPattern = new RegExp('[a-zA-z0-9]{2,30}@[a-zA-z]{3,15}.com?(.[a-z]{2})?', 'i') ;
-    const checkEmail = event => {
-        const value = event.target.value;
-        if(value) {
-            clearErrors('email');
-            let emailSuggestion = '';
-            Mailcheck.run({
-                email: value,
-                domains: domains,                       // optional
-                topLevelDomains: topLevelDomains,       // optional
-                //secondLevelDomains: secondLevelDomains, // optional
-                suggested: function(suggestion) {
-                    emailSuggestion = suggestion.full;
-                }
-            });
-            if((!emailSuggestion) && (emailPattern.test(value))) {
-                clearErrors('email');
-                setEmailSuggestions(e => '');
-            } else {
-                setError('email', { type: 'pattern', message: 'Invalid email address'});
-                setEmailSuggestions(e => emailSuggestion);
-            }
-        } else {
-            setError('email', { type: 'required', message: requiredMessage })
-        }
-    };
-
-    const labelClickHandler = async() => {
-        setValue('email', emailSuggestions);
-        clearErrors('email');
-        setEmailSuggestions(e => '');
-    };
-
-    const selectChangeHandler = event => setDayTime(event.target.value);
-
-    const getEmailHelperText = type => {
-        let helperText = '';
-
-        if(type === 'required') {
-            helperText = requiredMessage;
-        } else if(type === 'pattern') {
-            helperText = 'Invalid email address';
-        }
-
-        return helperText;
-    };
 
     useEffect(() => {
         window.scrollBy({ 
@@ -132,117 +45,7 @@ const Booking = () => {
                 </Grid>
                 <Container maxWidth={false} component="section"
                     className={classNames(classes.px, classes.bookingFormContainer)}>
-                    <Paper 
-                        component="form" autoComplete="off" 
-                        className={classNames(classes.bookingForm)} 
-                        onSubmit={handleSubmit(onSubmit)}>
-                        <TextField 
-                            label="Name" 
-                            fullWidth
-                            margin="normal"
-                            error={errors.name?.type === 'required'}
-                            helperText={errors.name?.type === 'required' ? requiredMessage : ''}
-                            { ...register('name', { required: requiredMessage})}
-                        />
-                        <Controller
-                            control={control} 
-                            name="email"
-                            defaultValue=""
-                            render={({ field }) => ( //{ ... register('email', { required: true }) }
-                                <TextField 
-                                    type="email"
-                                    label="Email"
-                                    fullWidth
-                                    margin="normal"
-                                    error={getEmailHelperText(errors.email?.type) ? true : false}
-                                    helperText={getEmailHelperText(errors.email?.type)}
-                                    { ...field}
-                                    onBlur={checkEmail}
-                                />
-                            )}
-                         />
-                        { emailSuggestions ? (
-                            <label 
-                                className={classNames(display.block, classes.emailSuggestion)}  onClick={labelClickHandler}>
-                                Do you mean <em className={classNames(classes.emailSuggestionHighlight)}>{emailSuggestions}</em>
-                            </label>) : '' 
-                        }
-                        <Paper elevation={0} className={classNames(classes.paper)}>
-                            <label className={classNames(classes.bookingLabel)}>Pick a day</label>
-                            <Paper elevation={0} className={classNames(display.flex, display.justifyBetween, classes.bookingFormPaper)}>
-                                <TextField 
-                                    label="DD" 
-                                    type="number"
-                                    className={classNames(classes.bookingInput)}
-                                    error={ errors.day?.type === 'required'  }
-                                    { ...register('day', { required: true }) }
-                                />
-                                <TextField 
-                                    label="MM" 
-                                    type="number"
-                                    className={classNames(classes.bookingInput)}
-                                    { ...register('month', { required: true }) }
-                                    error={ errors.month?.type === 'required' }
-                                />
-                                <TextField 
-                                    label="YYYY" 
-                                    type="number"
-                                    className={classNames(classes.bookingInput)}
-                                    { ...register('year', { required: true }) }
-                                    error={ errors.year?.type === 'required' }
-                                />
-                            </Paper>
-                        </Paper>
-                        <Paper elevation={0} className={classNames(classes.paper)}>
-                            <label className={classNames(classes.bookingLabel)}>Pick a time</label>
-                            <Paper elevation={0} className={classNames(display.flex, display.justifyBetween, classes.bookingFormPaper)}>
-                                <TextField 
-                                    label="MM" 
-                                    type="number"
-                                    className={classNames(classes.bookingInput)}
-                                    { ...register('hours', { required: true }) }
-                                    error={ errors.hours?.type === 'required' }
-                                />
-                                <TextField 
-                                    label="DD" 
-                                    type="number"
-                                    className={classNames(classes.bookingInput)}
-                                    { ...register('minutes', { required: true }) }
-                                    error={ errors.minutes?.type === 'required' }
-                                />
-                                <TextField 
-                                    select
-                                    label="select" 
-                                    className={classNames(classes.bookingInput)}
-                                    value={dayTime}
-                                    onChange={selectChangeHandler}
-                                    { ...register('dayTime', { required: true})}
-                                    error={ errors.dayTime?.type === 'required' }
-                                >
-                                    {options.map(option => (
-                                        <MenuItem key={option.value} value={option.value}>
-                                            {option.label}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </Paper>
-                        </Paper>
-                        <Paper elevation={0} 
-                            className={classNames(display.flex, display.justifyBetween, display.alignCenter, classes.paper, 
-                            classes.bookingActionsContainer)}>
-                            <Button className={classNames(classes.bookingActionButton)} onClick={decresaeGuestsNumber}>-</Button>
-                            <label 
-                                className={classNames(text.weight7, classes.bookingGuestsLabel)}>
-                                {guests} {guests === 1 ? 'person' : 'people'}
-                            </label>
-                            <Button className={classNames(classes.bookingActionButton)} onClick={incresaeGuestsNumber}>+</Button>
-                        </Paper>
-                        <Button 
-                            type="submit" 
-                            className={classNames(services.link, text.uppercase, classes.bookingSumbit)}>
-                            Make reservation
-                        </Button>
-                    </Paper>
+                    <Form />
                 </Container>
             </Container>
             <Footer />
