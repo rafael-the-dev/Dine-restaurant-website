08a38df8604050db07d1097a906aea9ecfff60de
rafael-tivane <luislangabusiness@gmail.com>
2021-08-24 07:32:24 +0100

    Wrapping components into useMemo function to avoid components re-creation in each re-render

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom';
 import {  useDisplay, useTypography, useServices } from '../../styles';
 import { useStyles } from './styles.js';
 import classNames from 'classnames';
+import { useMemo, useCallback } from 'react';
 
 const Form = () => {
     const classes = useStyles();
@@ -13,7 +14,7 @@ const Form = () => {
     const text = useTypography();
     const services = useServices();
 
-    const options = [{ value: 'AM', label: 'AM'}, { value: 'PM', label: 'PM'}];
+    const options = useMemo(() => [{ value: 'AM', label: 'AM'}, { value: 'PM', label: 'PM'}], [ ]);
     const [ guests, setGuests ] = useState(1); 
     const [ dayTime, setDayTime ] = useState('AM');
     const [ emailSuggestions, setEmailSuggestions ] = useState('');
@@ -24,22 +25,27 @@ const Form = () => {
 
     const history = useHistory();
 
-    const decresaeGuestsNumber = () => {
-        let number = guests  - 1;
-        if(number === 0) {
-            number = 1;
-        }
-        setGuests(g => number);
-    };
+    const decresaeGuestsNumber = useCallback(() => {
+        setGuests(oldNumber => {
+            let number = oldNumber  - 1;
 
-    const incresaeGuestsNumber = () => {
-        let number = guests  + 1;
+            if(number === 0) {
+                number = 1;
+            }
+            return number;
+        });
+    }, [  ]);
 
-        if(number === 8) {
-            number = 7;
-        }
-        setGuests(g => number);
-    };
+    const incresaeGuestsNumber = useCallback(() => {
+        setGuests(oldNumber => {
+            let number = oldNumber  + 1;
+
+            if(number === 8) {
+                number = 7;
+            }
+            return number;
+        });
+    }, [  ]);
 
     const handler = func => func();
 
@@ -54,14 +60,15 @@ const Form = () => {
         }
     };
 
-    const domains = ['gmail.com', 'aol.com', 'hotmail.com', 'yahoo.com', 'outlook.com', 'live.com', 'msn.com'];
-    const topLevelDomains = ["com", "net", "org"];
-    const emailPattern = new RegExp('[a-zA-z0-9]{2,30}@[a-zA-z]{3,15}.com?(.[a-z]{2})?', 'i') ;
-    const checkEmail = event => {
+    const checkEmail = useCallback(event => {
         const value = event.target.value;
         if(value) {
             clearErrors('email');
             let emailSuggestion = '';
+            const domains = ['gmail.com', 'aol.com', 'hotmail.com', 'yahoo.com', 'outlook.com', 'live.com', 'msn.com'];
+            const topLevelDomains = ["com", "net", "org"];
+            const emailPattern = new RegExp('[a-zA-z0-9]{2,30}@[a-zA-z]{3,15}.com?(.[a-z]{2})?', 'i') ;
+            
             Mailcheck.run({
                 email: value,
                 domains: domains,                       // optional
@@ -81,13 +88,13 @@ const Form = () => {
         } else {
             setError('email', { type: 'required', message: requiredMessage })
         }
-    };
+    }, [  setError, clearErrors,  ]);
 
-    const labelClickHandler = async() => {
+    const labelClickHandler = useCallback(async() => {
         setValue('email', emailSuggestions);
         clearErrors('email');
         setEmailSuggestions(e => '');
-    };
+    }, [ setValue, clearErrors, setEmailSuggestions, emailSuggestions ]);
 
     const selectChangeHandler = event => setDayTime(event.target.value);
 
@@ -103,118 +110,168 @@ const Form = () => {
         return helperText;
     };
 
+    const nameFieldMemo = useMemo(() => (
+        <TextField 
+            label="Name" 
+            fullWidth
+            margin="normal"
+            error={errors.name?.type === 'required'}
+            helperText={errors.name?.type === 'required' ? requiredMessage : ''}
+            { ...register('name', { required: requiredMessage})}
+        />
+    ), [  register, errors.name ]);
+
+    const emailFieldMemo = useMemo(() => (
+        <Controller
+            control={control} 
+            name="email"
+            defaultValue=""
+            rules={{ required: true, pattern: /[a-zA-z0-9]{2,30}@[a-zA-z]{3,15}\.com?(\.[a-z]{2})?/ }}
+            render={({ field }) => ( //{ ... register('email', { required: true }) }
+                <TextField 
+                    type="email"
+                    label="Email"
+                    fullWidth
+                    margin="normal"
+                    error={getEmailHelperText(errors.email?.type) ? true : false}
+                    helperText={getEmailHelperText(errors.email?.type)}
+                    { ...field }
+                    onBlur={checkEmail}
+                />
+            )}
+        />
+    ), [ errors.email, checkEmail, control ]);
+
+    const dayFieldMemo = useMemo(() => (
+        <TextField 
+            label="DD" 
+            type="number"
+            className={classNames(classes.bookingInput)}
+            error={ errors.day?.type === 'required'  }
+            { ...register('day', { required: true }) }
+        />
+    ), [ errors.day, register, classes ]);
+
+    const monthFieldMemo = useMemo(() => (
+        <TextField 
+            label="MM" 
+            type="number"
+            className={classNames(classes.bookingInput)}
+            { ...register('month', { required: true }) }
+            error={ errors.month?.type === 'required' }
+        />
+    ), [ register, errors.month, classes ]);
+
+    const yearFieldMemo = useMemo(() => (
+        <TextField 
+            label="YYYY" 
+            type="number"
+            className={classNames(classes.bookingInput)}
+            { ...register('year', { required: true }) }
+            error={ errors.year?.type === 'required' }
+        />
+    ), [ register, errors.year, classes ]);
+
+    const hoursFieldMemo = useMemo(() => (
+        <TextField 
+            label="MM" 
+            type="number"
+            className={classNames(classes.bookingInput)}
+            { ...register('hours', { required: true }) }
+            error={ errors.hours?.type === 'required' }
+        />
+    ), [ classes, register, errors.hours ]);
+
+    const minuteFieldMemo = useMemo(() => (
+        <TextField 
+            label="DD" 
+            type="number"
+            className={classNames(classes.bookingInput)}
+            { ...register('minutes', { required: true }) }
+            error={ errors.minutes?.type === 'required' }
+        />
+    ), [ classes, register, errors.minutes ]);
+
+    const selectFieldMemo = useMemo(() => (
+        <TextField 
+            select
+            label="select" 
+            className={classNames(classes.bookingInput)}
+            value={dayTime}
+            onChange={selectChangeHandler}
+            { ...register('dayTime', { required: true})}
+            error={ errors.dayTime?.type === 'required' }
+        >
+            {options.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                </MenuItem>
+            ))}
+        </TextField>
+    ), [ register, classes, errors.dayTime, options, dayTime ]);
+
+    const suggestionLabelMemo = useMemo(() => (
+        <label 
+            className={classNames(display.block, classes.emailSuggestion)}  onClick={labelClickHandler}>
+            Do you mean <em className={classNames(classes.emailSuggestionHighlight)}>{emailSuggestions}</em>
+        </label>
+    ), [ display, classes, emailSuggestions, labelClickHandler]);
+
+    const guestsLabelMemo = useMemo(() => (
+        <label 
+            className={classNames(text.weight7, classes.bookingGuestsLabel)}>
+            {guests} {guests === 1 ? 'person' : 'people'}
+        </label>
+    ), [ classes, text, guests ]);
+
+    const decreaseButtonMemo = useMemo(() => (
+        <Button className={classNames(classes.bookingActionButton)} onClick={decresaeGuestsNumber}>-</Button>
+    ), [ classes, decresaeGuestsNumber ]);
+
+    const increaseButtonMemo = useMemo(() => (
+        <Button className={classNames(classes.bookingActionButton)} onClick={incresaeGuestsNumber}>+</Button>
+    ), [ classes, incresaeGuestsNumber]);
+
+    const submitMemo = useMemo(() => (
+        <Button 
+            type="submit" 
+            className={classNames(services.link, text.uppercase, classes.bookingSumbit)}>
+            Make reservation
+        </Button>
+    ), [ services, text, classes ]);
+
     return (
         <Paper 
             component="form" autoComplete="off" 
             className={classNames(classes.bookingForm)} 
             onSubmit={onSubmit}>
-            <TextField 
-                label="Name" 
-                fullWidth
-                margin="normal"
-                error={errors.name?.type === 'required'}
-                helperText={errors.name?.type === 'required' ? requiredMessage : ''}
-                { ...register('name', { required: requiredMessage})}
-            />
-            <Controller
-                control={control} 
-                name="email"
-                defaultValue=""
-                rules={{ required: true, pattern: /[a-zA-z0-9]{2,30}@[a-zA-z]{3,15}\.com?(\.[a-z]{2})?/ }}
-                render={({ field }) => ( //{ ... register('email', { required: true }) }
-                    <TextField 
-                        type="email"
-                        label="Email"
-                        fullWidth
-                        margin="normal"
-                        error={getEmailHelperText(errors.email?.type) ? true : false}
-                        helperText={getEmailHelperText(errors.email?.type)}
-                        { ...field }
-                        onBlur={checkEmail}
-                    />
-                )}
-                />
-            { emailSuggestions ? (
-                <label 
-                    className={classNames(display.block, classes.emailSuggestion)}  onClick={labelClickHandler}>
-                    Do you mean <em className={classNames(classes.emailSuggestionHighlight)}>{emailSuggestions}</em>
-                </label>) : '' 
-            }
+            { nameFieldMemo }
+            { emailFieldMemo }
+            { emailSuggestions ? suggestionLabelMemo : '' }
             <Paper elevation={0} className={classNames(classes.paper)}>
                 <label className={classNames(classes.bookingLabel)}>Pick a day</label>
                 <Paper elevation={0} className={classNames(display.flex, display.justifyBetween, classes.bookingFormPaper)}>
-                    <TextField 
-                        label="DD" 
-                        type="number"
-                        className={classNames(classes.bookingInput)}
-                        error={ errors.day?.type === 'required'  }
-                        { ...register('day', { required: true }) }
-                    />
-                    <TextField 
-                        label="MM" 
-                        type="number"
-                        className={classNames(classes.bookingInput)}
-                        { ...register('month', { required: true }) }
-                        error={ errors.month?.type === 'required' }
-                    />
-                    <TextField 
-                        label="YYYY" 
-                        type="number"
-                        className={classNames(classes.bookingInput)}
-                        { ...register('year', { required: true }) }
-                        error={ errors.year?.type === 'required' }
-                    />
+                    { dayFieldMemo}
+                    { monthFieldMemo }
+                    { yearFieldMemo }
                 </Paper>
             </Paper>
             <Paper elevation={0} className={classNames(classes.paper)}>
                 <label className={classNames(classes.bookingLabel)}>Pick a time</label>
                 <Paper elevation={0} className={classNames(display.flex, display.justifyBetween, classes.bookingFormPaper)}>
-                    <TextField 
-                        label="MM" 
-                        type="number"
-                        className={classNames(classes.bookingInput)}
-                        { ...register('hours', { required: true }) }
-                        error={ errors.hours?.type === 'required' }
-                    />
-                    <TextField 
-                        label="DD" 
-                        type="number"
-                        className={classNames(classes.bookingInput)}
-                        { ...register('minutes', { required: true }) }
-                        error={ errors.minutes?.type === 'required' }
-                    />
-                    <TextField 
-                        select
-                        label="select" 
-                        className={classNames(classes.bookingInput)}
-                        value={dayTime}
-                        onChange={selectChangeHandler}
-                        { ...register('dayTime', { required: true})}
-                        error={ errors.dayTime?.type === 'required' }
-                    >
-                        {options.map(option => (
-                            <MenuItem key={option.value} value={option.value}>
-                                {option.label}
-                            </MenuItem>
-                        ))}
-                    </TextField>
+                    { hoursFieldMemo }
+                    { minuteFieldMemo }
+                    { selectFieldMemo }
                 </Paper>
             </Paper>
             <Paper elevation={0} 
                 className={classNames(display.flex, display.justifyBetween, display.alignCenter, classes.paper, 
                 classes.bookingActionsContainer)}>
-                <Button className={classNames(classes.bookingActionButton)} onClick={decresaeGuestsNumber}>-</Button>
-                <label 
-                    className={classNames(text.weight7, classes.bookingGuestsLabel)}>
-                    {guests} {guests === 1 ? 'person' : 'people'}
-                </label>
-                <Button className={classNames(classes.bookingActionButton)} onClick={incresaeGuestsNumber}>+</Button>
+                { decreaseButtonMemo }
+                { guestsLabelMemo}
+                { increaseButtonMemo }
             </Paper>
-            <Button 
-                type="submit" 
-                className={classNames(services.link, text.uppercase, classes.bookingSumbit)}>
-                Make reservation
-            </Button>
+            { submitMemo }
         </Paper>
     );
 };
